ac8a28448a0a90f9581c4349f5c814f8c9cdfe8c
Vitaliy S <lokizor@mail.ru>
2025-05-29 15:06:06 +0300

    Some minor fixes

--- models.py
+++ models.py
@@ -47,43 +47,67 @@ class FineTunedClassifier:
             model_name, num_labels=num_labels)
         
     def train(self, train_texts, train_labels, test_texts, test_labels, 
-              output_dir='./results', num_epochs=3, batch_size=16):
+              output_dir='./results', num_epochs=3, batch_size=16, learning_rate=2e-5):
         
         train_dataset = TransactionDataset(train_texts, train_labels, self.tokenizer)
         test_dataset = TransactionDataset(test_texts, test_labels, self.tokenizer)
         
-        training_args = TrainingArguments(
-            output_dir=output_dir,
-            num_train_epochs=num_epochs,
-            per_device_train_batch_size=batch_size,
-            per_device_eval_batch_size=batch_size,
-            warmup_steps=500,
-            weight_decay=0.01,
-            logging_dir='./logs',
-            logging_steps=10,
-            max_grad_norm=1.0,
-            gradient_accumulation_steps=2,
-            fp16=torch.cuda.is_available(),
-        )
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size)
         
-        trainer = Trainer(
-            model=self.model,
-            args=training_args,
-            train_dataset=train_dataset,
-            eval_dataset=test_dataset,
-        )
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=learning_rate)
+        total_steps = len(train_loader) * num_epochs
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=total_steps)
         
-        trainer.train()
-        self.trainer = trainer
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model.to(device)
         
-    def predict(self, texts):
+        self.model.train()
+        for epoch in range(num_epochs):
+            total_loss = 0
+            for batch in train_loader:
+                batch = {k: v.to(device) for k, v in batch.items()}
+                
+
+                outputs = self.model(**batch)
+                loss = outputs.loss
+                
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
+                
+                total_loss += loss.item()
+            
+            avg_loss = total_loss / len(train_loader)
+            print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {avg_loss:.4f}')
+        
+        self.model.save_pretrained(output_dir)
+        self.tokenizer.save_pretrained(output_dir)
+        
+        self.device = device
+        
+    def predict(self, texts, batch_size=16):
+        self.model.eval()
         dataset = TransactionDataset(texts, [0]*len(texts), self.tokenizer)
-        preds = self.trainer.predict(dataset)
-        return np.argmax(preds.predictions, axis=1)
+        data_loader = DataLoader(dataset, batch_size=batch_size)
+        
+        predictions = []
+        with torch.no_grad():
+            for batch in data_loader:
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+                
+                outputs = self.model(**batch)
+                logits = outputs.logits
+                batch_predictions = torch.argmax(logits, dim=-1)
+                predictions.extend(batch_predictions.cpu().numpy())
+        
+        return np.array(predictions)
     
-    def evaluate(self, texts, labels):
-        preds = self.predict(texts)
+    def evaluate(self, texts, labels, batch_size=16):
+        predictions = self.predict(texts, batch_size)
         return {
-            'f1': f1_score(labels, preds, average='weighted'),
-            'predictions': preds
+            'f1': f1_score(labels, predictions, average='weighted'),
+            'predictions': predictions
         }
